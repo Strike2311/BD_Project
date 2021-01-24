0122816a9a195389ee8dfed9bfc7b3b1e2851b2f
Strike2311 <49807482+Strike2311@users.noreply.github.com>
2021-01-24 16:15:43 +0100

    Initial commit

---
+++ src/Klient.java
@@ -0,0 +1,67 @@
+public class Klient {
+    private String imie;
+    private String nazwisko;
+    private String nr_telefonu;
+    private String miasto;
+    private String adres;
+    private String email;
+    private String haslo;
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public String getNr_telefonu() {
+        return nr_telefonu;
+    }
+
+    public void setNr_telefonu(String nr_telefonu) {
+        this.nr_telefonu = nr_telefonu;
+    }
+
+    public String getMiasto() {
+        return miasto;
+    }
+
+    public void setMiasto(String miasto) {
+        this.miasto = miasto;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHaslo() {
+        return haslo;
+    }
+
+    public void setHaslo(String haslo) {
+        this.haslo = haslo;
+    }
+
+
+}
